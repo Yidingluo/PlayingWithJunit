5829ea9c86fb93c2b7d9cac452304e09372928f3
isunrmit <ivan.sun@rmit.edu.au>
2021-08-02 13:01:14 +1000

    Final updates for MyCalculator 01

--- src/test/java/edu/au/rmit/ct/MyCalculatorTest.java
+++ src/test/java/edu/au/rmit/ct/MyCalculatorTest.java
@@ -5,25 +5,52 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class MyCalculatorTest {
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		System.out.println("Setting up Before Class");
 	}
 
 	@BeforeEach
 	void setUp() throws Exception {
+		System.out.println("Setting up before each test");
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
+		System.out.println("Tearing Down After each test");
 	}
 
 	@Test
 	void test() { //Generic test fail
 		fail("Not yet implemented");
 	}
+	
+	/*
+	 *  @Test
+	 *  void testAdditionOfIntegersSimple() {}
+	 */
+
+	/*
+	 *  @Test
+	 *  void testDivideTwoIntegers() {}
+	 */
+
+	/*
+	 *  @Test
+	 *  void testModulusTwoIntegers() {}
+	 */
+	
+	/*
+	 *  @Test
+	 *  void testExpectedExceptionWhenDivideByZero() {}
+	 */
+	
+ 
+	
 
 }
